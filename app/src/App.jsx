3e0ffcbce3a77a839c34d32cf37b026dd6867754
heoh0613 <heoh0613@gmail.com>
2023-07-04 16:47:11 +0900

    FE_feat/ButtonComponent correction

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,8 +1,19 @@
 import './App.css';
+import DisabledButton from './Components/Button/DisabledButton';
+import MainButton from './Components/Button/MainButton';
+import OutlineButton from './Components/Button/OutlineButton';
+import SubButton from './Components/Button/SubButton';
 
 function App() {
 
-  return <></>;
+  return (
+  <>
+    <MainButton></MainButton>
+    <SubButton></SubButton>
+    <OutlineButton></OutlineButton>
+    <DisabledButton></DisabledButton>
+  </>
+  );
 }
 
 export default App;
