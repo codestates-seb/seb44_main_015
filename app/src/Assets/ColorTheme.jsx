b5e974a13791989a81c3c20126605f9dca4ca5bb
ujujungbuck <garam8u2@gmail.com>
2023-07-05 11:54:50 +0900

    Fix : conflict2

--- app/src/Assets/ColorTheme.jsx
+++ app/src/Assets/ColorTheme.jsx
@@ -8,4 +8,30 @@ export const Colors = {
   Gray3: '#999999',
   Gray4: '#333333',
   Bgwhite: '#ffffff',
+  Error: '#ff2727',
+};
+
+export const Messages = {
+  default: '콘텐츠 준비중입니다.',
+
+  cardInTitle: '명함 넣은 회사',
+  cardInMessage: '아직 명함을 넣은 회사가 없어요!',
+
+  bookmarkedTitle: '북마크한 회사',
+  bookmarkedMessage: '북마크한 회사가 없어요!',
+
+  selectedTitle: '채택된 명함',
+  openTitle: '열린 채용',
+  closedTitle: '지난 채용',
+
+  careeringMessage: '채용 중인 회사들을 보시겠어요?',
+  careerUpMessage: '채용을 올려보시겠어요?',
+
+  noCareerMessage: '지난채용이 없습니다',
+  cardEditBtn: '명함·이력 수정하기',
+  companyCardEditBtn: '회사 정보 수정하기',
+  showCareerBtn: '채용 중인 회사 보기',
+  plusCareerBtn: '+ 채용 추가하기',
+  plusResumeBtn: '+ 이력 추가하기',
+  editBtn: '수정하기',
 };
