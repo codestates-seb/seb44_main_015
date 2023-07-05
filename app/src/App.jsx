5e99ade14b745bf0af35747ddd0663ce38b40421
cggo.89@gmail.com <leogo@Leoui-MacBookPro.local>
2023-07-05 17:16:47 +0900

    [FE] Feat: Footer UI 완성

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,7 @@
 import './App.css';
-import Footer from './Components/Commons/Layouts/Footer';
 
 function App() {
-  return (
-    <>
-      <Footer></Footer>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
