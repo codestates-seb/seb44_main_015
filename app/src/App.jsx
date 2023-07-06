a71d3415e510836f8d66755b57cf537cbd3321f2
cggo.89@gmail.com <leogo@Leoui-MacBookPro.local>
2023-07-06 16:37:16 +0900

    진행중

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,7 +1,21 @@
 import './App.css';
-
+import NameCard from './Components/Commons/NameCard';
+import CompanyDetail from './Components/Commons/CompanyDetail';
+import NoLineTag from './Components/Commons/NoLineTag';
+import Resume from './Components/Commons/Resume';
+import Tag from './Components/Commons/Tag';
+import TextArea from './Components/Commons/TextArea';
 function App() {
-  return <></>;
+  return (
+    <>
+      {/* <NameCard></NameCard>
+      <CompanyDetail></CompanyDetail> */}
+      {/* <NoLineTag>dsd</NoLineTag> */}
+      {/* <Resume></Resume> */}
+      <Tag>dsasad</Tag>
+      {/* <TextArea></TextArea> */}
+    </>
+  );
 }
 
 export default App;
--- app/src/Components/Commons/TextArea.jsx
+++ app/src/Components/Commons/TextArea.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Colors } from '../../Assets/ColorTheme';
+import { Colors } from '../../Assets/Theme';
 
 function TextArea({
   title,
