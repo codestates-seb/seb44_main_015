a16848972391b10927b7d228b64609e89d97e944
nkower <sbgofspeed@naver.com>
2023-07-06 22:27:17 +0900

    [BE]feat security login

---
+++ server/src/main/java/main/security/jwt/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,106 @@
+package main.security.jwt.filter;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import main.company.entity.Company;
+import main.security.jwt.JwtTokenizer;
+import main.security.jwt.dto.LoginDto;
+import main.security.jwt.dto.TokenResponseDto;
+import main.user.entity.User;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+//    private static final String CONTENT_TYPE = "application/json";
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenizer jwtTokenizer;
+
+
+    @SneakyThrows
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        LoginDto loginDto = objectMapper.readValue(request.getInputStream(), LoginDto.class);
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginDto.getUserType()+":"+loginDto.getEmail(), loginDto.getPassword());
+
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws ServletException, IOException {
+        User user = (User) authResult.getPrincipal();
+
+        String accessToken = delegateAccessToken(user);
+        String refreshToken = delegateRefreshToken(user);
+        Long userId = user.getUserId();
+        Integer expiration = jwtTokenizer.getAccessTokenExpirationMinutes();
+
+        response.setHeader("Authorization", "Bearer"+ accessToken);
+        response.setHeader("Refresh", refreshToken);
+        Gson gson = new Gson();
+        response.getWriter().println(gson.toJson(new TokenResponseDto(userId, expiration, accessToken, refreshToken)));
+
+        this.getSuccessHandler().onAuthenticationSuccess(request, response, authResult);
+    }
+
+    private String delegateAccessToken(User user){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("email", user.getEmail());
+        claims.put("roles", user.getRoles());
+        claims.put("userId", user.getUserId());
+
+        String subject = user.getEmail();
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getAccessTokenExpirationMinutes());
+
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+
+        String accessToken = jwtTokenizer.generateAccessToken(claims, subject, expiration, base64EncodedSecretKey);
+
+        return accessToken;
+    }
+/*
+    private String delegateAccessToken(Company company){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("email", company.getEmail());
+        claims.put("roles", company.getRoles());
+        claims.put("id", company.getCompanyId());
+
+        String subject = user.getEmail();
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getAccessTokenExpirationMinutes());
+
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+
+        String accessToken = jwtTokenizer.generateAccessToken(claims, subject, expiration, base64EncodedSecretKey);
+
+        return accessToken;
+    }
+    */
+
+    private String delegateRefreshToken(User user){
+        String subject = user.getEmail();
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getRefreshTokenExpirationMinutes());
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+
+        String refreshToken = jwtTokenizer.generateRefreshToken(subject, expiration, base64EncodedSecretKey);
+
+        return refreshToken;
+    }
+}
