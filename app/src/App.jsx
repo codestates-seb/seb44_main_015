f0e00e3743bee7cc23bc73f12c49dcb1e21c8141
cggo.89@gmail.com <leogo@Leoui-MacBookPro.local>
2023-07-06 15:55:03 +0900

    FE_feat/Header

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css';
+import Footer from './Components/Commons/Layouts/Footer';
+import Header from './Components/Commons/Layouts/Header';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
