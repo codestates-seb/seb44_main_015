d0728460181ea15b0d01ed295468a60dbe88f166
heoh06 <124794790+heoh06@users.noreply.github.com>
2023-07-06 17:59:46 +0900

    App.jsx 기본값으로 수정

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,7 @@
 import './App.css';
-import SelectBox from './Components/Commons/SelectBox';
 
 function App() {
-  return (<>
-  <SelectBox name={'메인 산업군'}/>  
-  </>);
+  return <></>;
 }
 
 export default App;
