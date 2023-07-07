599027313e1bd883af1801e003cc683d643563a7
ujujungbuck <garam8u2@gmail.com>
2023-07-07 16:58:02 +0900

    Refactor : section flex정리

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,7 @@
 import './App.css';
-import MyPageCompany from './Pages/MyPageCompany';
-import MyPageFreelancer from './Pages/MyPageFreelancer';
 
 function App() {
-  return (
-    <>
-      <MyPageCompany />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
