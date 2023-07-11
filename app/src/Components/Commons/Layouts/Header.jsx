192046dfb2a8853c7e56c87b4655511fdea24c3e
erirumble <124761623+erirumble@users.noreply.github.com>
2023-07-11 18:29:42 +0900

    Revert "[FE] Feat: Employment Card 기본 UI 완성"

--- app/src/Components/Commons/Layouts/Header.jsx
+++ app/src/Components/Commons/Layouts/Header.jsx
@@ -102,8 +102,7 @@ const SignupStyled = styled(LoginStyled)`
   width: 50px;
   margin-left: 9px;
   padding-left: 9px;
-  border-left: 1px solid ${Colors.Gray3};
-
+  border-left: 1px solid ${Colors.Gray4};
 `;
 
 const ProfileStyled = styled.img`
