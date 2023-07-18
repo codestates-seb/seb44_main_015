5ccf5b9d7860cf56ed60eb29de95ad3fe9b456b5
nkower <sbgofspeed@naver.com>
2023-07-18 15:35:19 +0900

    검색기능 추가

--- server/src/main/java/main/noticeTag/repository/NoticeTagRepository.java
+++ server/src/main/java/main/noticeTag/repository/NoticeTagRepository.java
@@ -1,11 +1,15 @@
 package main.noticeTag.repository;
 
+import main.notice.entity.Notice;
 import main.noticeTag.entity.NoticeTag;
 import main.noticeTag.entity.NoticeTag;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface NoticeTagRepository extends JpaRepository<NoticeTag, Long> {
     Optional<NoticeTag> findByNoticeNoticeIdAndTagTagId(Long noticeId, Long tagId);
+    List<NoticeTag> findAllByTagNameOrderByNoticeCreatedAtDesc(String name, Pageable pageable);
 }
--- server/src/main/java/main/tag/controller/TagController.java
+++ server/src/main/java/main/tag/controller/TagController.java
@@ -1,6 +1,9 @@
 package main.tag.controller;
 
 import lombok.RequiredArgsConstructor;
+import main.notice.entity.Notice;
+import main.notice.mapper.NoticeMapper;
+import main.notice.service.NoticeService;
 import main.tag.dto.TagDto;
 import main.tag.entity.Tag;
 import main.tag.mapper.TagMapper;
@@ -20,6 +23,8 @@ import java.util.List;
 public class TagController {
     private final TagService tagService;
     private final TagMapper tagMapper;
+    private final NoticeService noticeService;
+    private final NoticeMapper noticeMapper;
     @PostMapping
     public ResponseEntity postTag(@Valid @RequestBody TagDto.Post tagPostDto){
         tagService.createTag(tagMapper.tagPostDtoToTag(tagPostDto));
@@ -32,4 +37,13 @@ public class TagController {
 
         return new ResponseEntity(tagMapper.tagsToTagResponses(tags), HttpStatus.OK);
     }
+
+    @GetMapping("/search")
+    public ResponseEntity getTagsSearch(@RequestParam(required = false, defaultValue = "10") int limit,
+                                        @RequestParam(required = false, defaultValue = "0") int page,
+                                        @RequestParam String tag){
+        List<Notice> notices = noticeService.findNoticesPage(tag, page, limit);
+        return new ResponseEntity<>(noticeMapper.noticesToNoticeResponseDtos(notices),HttpStatus.OK);
+    }
+
 }
